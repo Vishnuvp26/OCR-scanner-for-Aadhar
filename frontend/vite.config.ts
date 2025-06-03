bea8ec35f5447fa597e98b8ee51948dc4adfe3e5
Vishnu Mohan <vishnumohanmfc@gmail.com>
2025-06-03 17:09:56 +0530

    chore: added verce.json for deloyment

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,16 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath } from 'url'
+import { dirname, resolve } from 'path'
 
-// https://vite.dev/config/
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": resolve(__dirname, "./src"),
     },
   },
 })
